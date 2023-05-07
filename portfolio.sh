93587c1da816fbf8796443ae0c29d2d9b74693b2
mo-nguyen <mo.nguyen.tmo@gmail.com>
2023-05-07 22:18:21 +0700

    update

--- portfolio.sh
+++ portfolio.sh
@@ -3,5 +3,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
-(gunicorn porfolio.wsgi --user www-data --bind 0.0.0.0:8001 --workers 3) &
+(gunicorn portfolio.wsgi --user www-data --bind 0.0.0.0:8001 --workers 3) &
 nginx -g "daemon off;"
