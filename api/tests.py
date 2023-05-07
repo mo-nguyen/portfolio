55dbae87272725e2530e05985142f5d4fe4f42c4
mo-nguyen <mo.nguyen.tmo@gmail.com>
2023-05-07 19:27:14 +0700

    add test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from api.models import Tag, Project
 # Create your tests here.
+
+class TagTestCase(TestCase):
+    def test_tag(self):
+        tag = Tag(name="Test")
+        self.assertEqual(tag.name, "Test")
+
+
+class ProjectTestCase(TestCase):
+    def test_project(self):
+        project = Project(title="Test", url="http://localhost", description="description")
+        self.assertEqual(project.title, "Test")
+        self.assertEqual(project.url, "http://localhost")
+        self.assertEqual(project.description, "description")
